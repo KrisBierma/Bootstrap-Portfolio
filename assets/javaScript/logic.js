1bfe5f1b23f39c7a402d5a75adee1a841634785c
Kris <kris.bierma@gmail.com>
2018-04-04 16:49:04 -0500

    added node.js command line screen shots to portfolio page

--- assets/javaScript/logic.js
+++ assets/javaScript/logic.js
@@ -1,41 +1,82 @@
 $(document).ready(function(){
     var hangman={
+        name:"hangman",
         location:$("#hangman"),
         techs:["bootstrap", "javaScript", "css"]
         };
 
     var crystals={
+        name:"crystals",
         location:$("#crystals"),
         techs:["javaScript", "jQuery", "css"]
         };
     var trivia={
+        name:"trivia",
         location:$("#trivia"),
         techs:["javaScript","css"]
-    }
+        }
     var train={
+        name:"train",
         location:$("#train"),
-        techs:["javaScript", "jQuery", "moment", "firebse", "css"]
-    }
+        techs:["javaScript", "jQuery", "moment", "firebase", "css"]
+        }
+    var node={
+        name:"nodejs",
+        location:$("#node"),
+        techs:["javaScript", "Node.js", "Command-Line"]
+        }
      
-  
-    var portPics=[hangman, crystals, trivia, train];
+    var portPics=[hangman, crystals, trivia, train, node];
     var included;
     var currentBtn;
 
+    //show all apps on all button
     $("#all").on("click", function(){
         showBtn();
     });
 
-    $("#javaScript").on("click", function(){
+    //add transition-delay to port-tags when hovering
+    var currentCard;
+    $(".card").one("mouseover", function(){
+        currentCard = this.id;
+        console.log(currentCard);
+        for (var i=0; i<portPics.length; i++){
+            if (portPics[i].name === currentCard){
+                currentCard = portPics[i];
+                console.log(currentCard);
+            }
+        }
+        // console.log(this);
+        console.log(currentCard.techs);
+        for (var i=0; i<currentCard.techs.length; i++){
+            console.log("calling"+currentCard.techs[i]);
+            var addDrop = setInterval
+            (transitionDown,1000);
+        }
+    });
+
+    function transitionDown (){
+        // for (techs in currentCard){
+            console.log(currentCard.techs);
+            $(".port-tags-wrapper li").addClass("transD");
+        // }
+    }
+
+    //turn off port-tags dropping down when mouse is off
+    $(".card").on("mouseout", function(){
+        $(".port-tags-wrapper li").removeClass("transD");
+
+    })
+
+    //show apps using this button's technology
+    $(".portButtons").on("click", function(){
         currentBtn = this.id;
+                console.log(currentBtn);
+
         hideBtn(currentBtn);
     });
 
-    $("#jQuery").on("click", function(){
-        currentBtn = this.id;
-        hideBtn(currentBtn);
-    });  
-
+    //show buttons with the specified technology
     function showBtn(){
         for (var i=0; i<portPics.length; i++){
             var currentPort=portPics[i];
@@ -44,6 +85,7 @@ $(document).ready(function(){
         }
     }
 
+    //hide buttons that don't have specified tech
     function hideBtn(){
         showBtn();
         // console.log(currentBtn);
@@ -64,6 +106,23 @@ $(document).ready(function(){
             }
         }
     }
+
+    //show "to the top" button when scrolling
+    window.onscroll=function(){
+        if (      document.documentElement.scrollTop > 500){
+            $("#topBtn").addClass("topBtnShow").removeClass("hide");
+        }
+        else {
+            $("#topBtn").addClass("hide").removeClass("topBtnShow");
+        }
+    }
+
+    //take user to the top of the page
+    $("#topBtn").on("click", function(){
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop=0;
+    });
+
 
     
 })
